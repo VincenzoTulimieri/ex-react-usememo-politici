28144ce8a83a27c4a8d01493326ca42a0b5d1525
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-08-17 22:39:32 +0200

    feat: installazione bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 
-
+async function fetchJson(url) {
+  const response = await fetch(url)
+  const obj = response.json()
+  return obj;
+}
 function App() {
+  const [politicans, setPoliticans] = useState(null)
+
+  useEffect(() => {
+    async function promise() {
+      const politicans = await fetchJson('http://localhost:3333/politicians')
+      return politicans
+    }
+    (async () => {
+      const politicans = await promise()
+      return setPoliticans(politicans)
+    })()
+  }, [])
+
+
 
   return (
     <>
-      
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            Column
+          </div>
+          <div class="col">
+            Column
+          </div>
+          <div class="col">
+            Column
+          </div>
+        </div>
+      </div>
     </>
   )
 }
