568556fd91062b34694ff10a44252bbdbf8e0fee
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-08-17 23:51:57 +0200

    feat: card con politici

--- src/App.jsx
+++ src/App.jsx
@@ -20,20 +20,23 @@ function App() {
   }, [])
 
 
-
   return (
     <>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col">
-            Column
-          </div>
-          <div class="col">
-            Column
-          </div>
-          <div class="col">
-            Column
-          </div>
+      <div className="container text-center">
+        <h1>Lista dei politici</h1>
+        <div className="row">
+          {politicans && politicans.map(politican => (
+          <div className="col-4 gy-3" key={politican.id}>
+                <div className="card h-100 " >
+                  <img src={politican.image} className="card-img-top" alt={politican.name} />
+                  <div className="card-body">
+                    <p className="card-text"><strong>Name:</strong> {politican.name}</p>
+                    <p className="card-text"><strong>Political Office</strong>: {politican.position}</p>
+                    <p className="card-text"><strong>Biografy:</strong> {politican.biography}</p>
+                  </div>
+                </div>
+              </div>
+          ))}
         </div>
       </div>
     </>
